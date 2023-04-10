e95cd25d010a0a50e469fbff7612a7ea202fddfc
jackrhines <jrhines@calpoly.edu>
2023-04-10 12:59:10 -0700

    implemented delete method & querying by Name&Job; cleaned up file

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,90 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/users', (req, res) => {
+    const name = req.query.name;
+    const job = req.query.job;
+
+    if (name !== undefined && job !== undefined){
+        let result = findUserByNameAndJob(name, job)
+        result = {users_list: result};
+        res.send(result)
+    }
+    else if (name !== undefined){
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else if (job !== undefined){
+        let result = findUserByJob(job);
+        result = {users_list: result};
+        res.send(result)
+    }
+    else{
+        res.send(users);
+    }
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        result = {users_list: result};
+        res.send(result);
+    }
+});
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.status(200).end();
+});
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    deleteUserById(id);
+    res.status(200).end();
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+const findUserByName = (name) => {
+    return users['users_list'].filter( (user) => user['name'] === name);
+}
+
+const findUserByJob = (job) => {
+    return users['users_list'].filter((user) => user['job'] === job);
+}
+
+const findUserByNameAndJob = (name, job) => {
+    return users['users_list'].filter((user) =>
+        (user['name'] === name && user['job'] === job))
+}
+
+function findUserById(id) {
+    return users['users_list'].find( (user) => user['id'] === id); // or line below
+    //return users['users_list'].filter( (user) => user['id'] === id);
+}
+
+function addUser(user){
+    users['users_list'].push(user);
+}
+
+function deleteUserById(id) {
+    users['users_list'] = users['users_list'].filter( (user) => user['id'] !== id);
+
+}
+
 const users = {
     users_list :
         [
@@ -23,7 +107,7 @@ const users = {
             {
                 id: 'yat999',
                 name: 'Dee',
-                job: 'Aspring actress',
+                job: 'Bird',
             },
             {
                 id: 'zap555',
@@ -32,56 +116,3 @@ const users = {
             }
         ]
 }
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.get('/users', (req, res) => {
-    const name = req.query.name;
-    if (name !== undefined){
-        let result = findUserByName(name);
-        result = {users_list: result};
-        res.send(result);
-    }
-    else{
-        res.send(users);
-    }
-});
-
-const findUserByName = (name) => {
-    return users['users_list'].filter( (user) => user['name'] === name);
-}
-
-app.get('/users/:id', (req, res) => {
-    const id = req.params['id']; //or req.params.id
-    let result = findUserById(id);
-    if (result === undefined || result.length == 0)
-        res.status(404).send('Resource not found.');
-    else {
-        result = {users_list: result};
-        res.send(result);
-    }
-});
-
-function findUserById(id) {
-    return users['users_list'].find( (user) => user['id'] === id); // or line below
-    //return users['users_list'].filter( (user) => user['id'] === id);
-}
-
-app.post('/users', (req, res) => {
-    const userToAdd = req.body;
-    addUser(userToAdd);
-    res.status(200).end();
-});
-
-function addUser(user){
-    users['users_list'].push(user);
-}
-
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
