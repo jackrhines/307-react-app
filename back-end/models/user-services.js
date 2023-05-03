0eeed01a9411812e1a0ac12c232feae0bebf0935
jackrhines <jrhines@calpoly.edu>
2023-05-03 16:00:28 -0700

    app functions with a remote server

--- back-end/models/user-services.js
+++ back-end/models/user-services.js
@@ -1,12 +1,27 @@
 const mongoose = require("mongoose");
 const userModel = require("./user");
+const dotenv = require("dotenv");
 mongoose.set("debug", true);
 
+dotenv.config();
+
 mongoose
-  .connect("mongodb://localhost:27017/users", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://" +
+    process.env.MONGO_USER +
+    ":" +
+    process.env.MONGO_PWD +
+    "@" +
+    process.env.MONGO_CLUSTER +
+    "/" +
+    process.env.MONGO_DB +
+    "?retryWrites=true&w=majority",
+    // "mongodb://localhost:27017/users",
+    {
+      useNewUrlParser: true, //useFindAndModify: false,
+      useUnifiedTopology: true,
+    }
+  )
   .catch((error) => console.log(error));
 
 async function getUsers(name, job) {
